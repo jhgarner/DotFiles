8e4c69d244fdff597a29834c550912b2f1f42208
Jack <jacksonhgarner@outlook.com>
2025-09-12 09:04:45 -0700

    Add laptop stuff

--- configuration.nix
+++ configuration.nix
@@ -1,8 +1,8 @@
 # Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, lib, pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   imports =
@@ -11,46 +11,44 @@
     ];
 
   environment.pathsToLink = ["/include" "/lib"];
-  nixpkgs.overlays = [
-    (self: super: {
-      cascadia-code = pkgs.fetchzip {
-        postFetch = ''
-          mkdir -p $out/share/fonts/
-          unzip -j $downloadedFile \*.otf -d $out/share/fonts/opentype
-          unzip -j $downloadedFile \*.ttf -d $out/share/fonts/truetype
-        '';
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  # environment.ldso = "${pkgs.clang.libc_bin}/bin/ld.so";
+  # environment.ldso32 = "${pkgs.clang.libc_bin}/bin/ld.so";
 
-        name = "cascadia-code-2102.25";
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-        url = "https://github.com/microsoft/cascadia-code/releases/download/v2102.25/CascadiaCode-2102.25.zip";
-
-        sha256 = "14qhawcf1jmv68zdfbi2zfqdw4cf8fpk7plxzphmkqsp7hlw9pzx";
-      };
-    })
-  ];
-  services.udev.packages = [ pkgs.android-udev-rules ];
-  programs.adb.enable = true;
-
-  networking.networkmanager.enable = true; # Enables wireless support via NetworkManager
-
+  # Enable networking
+  networking.networkmanager.enable = true;
   hardware.graphics.enable32Bit = true;
-  boot.loader.systemd-boot.memtest86.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
-  # A whole bunch of programs to install
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  programs.kdeconnect.enable = true;
-  programs.kdeconnect.package = pkgs.gnomeExtensions.gsconnect;
-
-
 
   environment.systemPackages = with pkgs;
   [
       # Desktop
-      gnome-tweaks
+      # gnome-tweaks
       i3
       rofi
       dunst
@@ -66,6 +64,13 @@
       dmenu
       i3status
       barrier
+      mako
+      fuzzel
+      swaylock
+      waybar
+      xwayland-satellite
+      overskride
+      nautilus
       # Gui
       kitty
       alacritty
@@ -99,8 +104,10 @@
       wget
       mkpasswd
       starship
+      bc
       git
       git-secret
+      gh
       clang.bintools
       lazygit
       file
@@ -141,6 +148,8 @@
       nodejs
       python3
       rustup
+      nil
+      jq
       # Libs
       ncurses
       zlib
@@ -209,7 +218,8 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   # programs.gnome-terminal.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+  programs.niri.enable = true;
   # services.displayManager.sddm.enable = true;
   # services.desktopManager.plasma6.enable = true;
   # services.displayManager.sddm.wayland.enable = true;
@@ -225,7 +235,7 @@
     home = "/home/jack";
     hashedPassword = "$6$0ezGsg1cC$KoCnA3QoxZ0UzF2gAOca3m9Ura8Gnb389RJg5XWcqTdMI3t6sJUJrlSPyUOwuh2v401L2a5Ot5Pv4OfgaHcGE1";
     group = "jack";
-    extraGroups = [ "testing" "wheel" "networkmanager" "video" "adbusers" "docker" "wireshark" "plugdev" "input" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "testing" "wheel" "networkmanager" "video" "adbusers" "docker" "wireshark" "plugdev" "input" "uinput" ]; # Enable ‘sudo’ for the user.
     shell = pkgs.zsh;
   };
   users.groups = { jack = { }; };
@@ -269,7 +279,7 @@
   # services.redshift.enable = true;
   # services.redshift.executable = "/bin/redshift-gtk";
 
-  programs.ssh.startAgent = true;
+  # programs.ssh.startAgent = true;
 
   programs.java.enable = true;
 
@@ -291,122 +301,98 @@
   programs.mosh.enable = true;
 
   networking.nat.enable = true;
-  networking.nat.externalInterface = "enp9s0";
   networking.nat.internalInterfaces = [ "wg0" ];
-  networking.wireguard.interfaces = {
-    # "wg0" is the network interface name. You can name the interface arbitrarily.
-    wg0 = {
-      # Determines the IP address and subnet of the server's end of the tunnel interface.
-      ips = [ "10.100.0.1/24" ];
-
-      # The port that WireGuard listens to. Must be accessible by the client.
-      listenPort = 51821;
-
-      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-      # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-      postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o enp9s0 -j MASQUERADE
-      '';
-
-      # This undoes the above command
-      postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o enp9s0 -j MASQUERADE
-      '';
-
-      # Path to the private key file.
-      #
-      # Note: The private key can also be included inline via the privateKey option,
-      # but this makes the private key world-readable; thus, using privateKeyFile is
-      # recommended.
-      privateKeyFile = "/home/jack/wireguard-keys/private";
-
-      peers = [
-        # List of allowed peers.
-        {
-          # Phone
-          # Public key of the peer (not a file path).
-          publicKey = "m/BtoupKxcEYBj3v1GE6laLIOXObLslgZLsY/S1qyEk=";
-          # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
-          allowedIPs = [ "10.100.0.2/32" ];
-        }
-        {
-          # Phone
-          # Public key of the peer (not a file path).
-          publicKey = "/vilJboxgJEn04D5vKYL7Wd6F2Qi91etDUfxEGbFLzY=";
-          # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
-          allowedIPs = [ "10.100.0.3/32" ];
-        }
-      ];
-    };
-  };
   services.udev.extraRules = ''
     KERNEL=="uinput", GROUP="input", MODE="0660", OPTIONS+="static_node=uinput"
   '';
   # systemd.tmpfiles.rules = [ "L+ /usr/ - - - - /run/current-system/sw/" ];
   services.flatpak.enable = true;
-  # services.vscode-server.enable = true;
-  services.acpid.enable = lib.mkForce false;
-  services.sysprof.enable = true;
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "jack";
-  services.displayManager.preStart = "sleep 5";
-  networking.hostName = "jackDesktop"; # Define your hostname.
-  # programs.nix-ld.enable = true;
-  # programs.nix-ld.libraries = with pkgs; [
-  #   gmp
-  #   ncurses
-  #   zlib
-  #   zstd
-  #   clang.cc.lib
-  #   (runCommand "copied-gcc" {} ''
-  #       mkdir -p $out/
-  #       cd $out
-  #       cp -r -L ${gcc.cc.out}/. ./
-  #       chmod -R +w .
-  #       cp -r -L ${gcc.cc.lib}/. ./
-  #       chmod -R +w .
-  #       cp -r -L ${libcxx.dev}/include/c++/v1/. ./include/c++
-  #       chmod -R +w .
-  #   '')
-  #   clang.cc.out
-  #   clang.libc
-  #   # libcxx.out
-  #   # libcxx.dev
-  #   # clang.libcxx
-  #   # stdenv.cc.cc
-  #   curl
-  #   openssl
-  #   attr
-  #   libssh
-  #   bzip2
-  #   libxml2
-  #   acl
-  #   libsodium
-  #   util-linux
-  #   xz
-  #   systemd
-  #   ncurses
-  #   xorg.libxcb
-  #   xorg.xcbutilwm
-  # ];
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "23.11"; # Did you read the comment?
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    gmp
+    ncurses
+    zlib
+    zstd
+    clang.cc.lib
+    (runCommand "copied-gcc" {} ''
+        mkdir -p $out/
+        cd $out
+        cp -r -L ${gcc.cc.out}/. ./
+        chmod -R +w .
+        cp -r -L ${gcc.cc.lib}/. ./
+        chmod -R +w .
+        cp -r -L ${libcxx.dev}/include/c++/v1/. ./include/c++
+        chmod -R +w .
+    '')
+    clang.cc.out
+    clang.libc
+    libcxx.out
+    # libcxx.dev
+    # clang.libcxx
+    # stdenv.cc.cc
+    curl
+    openssl
+    attr
+    libssh
+    bzip2
+    libxml2
+    acl
+    libsodium
+    util-linux
+    xz
+    systemd
+    ncurses
+    xorg.libxcb
+    xorg.xcbutilwm
+  ];
+
+  # xremap
+  
+  systemd.user.services.xremap =
+    let
+      toggleWaybar = ["${pkgs.procps}/bin/pkill" "-SIGUSR1" "waybar"];
+      config = {
+        modmap = [
+          {
+            remap = {
+              LeftMeta.press.launch = toggleWaybar;
+              LeftMeta.release.launch = toggleWaybar;
+            };
+          }
+        ];
+        keymap = [];
+      };
+      start = "${pkgs.xremap}/bin/xremap ${(pkgs.formats.yaml {}).generate "xremapConfig.yml" config}";
+    in {
+      description = "xremap user service";
+      path = [ pkgs.xremap ];
+      wantedBy = [ "graphical-session.target" ];
+      serviceConfig = lib.mkMerge [
+        {
+          KeyringMode = "private";
+          SystemCallArchitectures = [ "native" ];
+          RestrictRealtime = true;
+          ProtectSystem = true;
+          SystemCallFilter = map (x: "~@${x}") [
+            "clock"
+            "debug"
+            "module"
+            "reboot"
+            "swap"
+            "cpu-emulation"
+            "obsolete"
+            # NOTE: These two make the spawned processes drop cores
+            # "privileged"
+            # "resources"
+          ];
+          LockPersonality = true;
+          UMask = "077";
+          RestrictAddressFamilies = "AF_UNIX";
+          ExecStart = start;
+        }
+      ];
+    };
+
 
 }
-
